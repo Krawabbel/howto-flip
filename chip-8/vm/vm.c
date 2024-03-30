b06e6063c78ddbbfc6ad78a36fe6b5310076647a
Krawabbel <drkrawabbel@gmail.de>
2024-03-30 12:48:06 +0100

    basic implementation of vm ready, corax and other tests pass

--- chip-8/vm/vm.c
+++ chip-8/vm/vm.c
@@ -12,7 +12,10 @@ typedef uint16_t word;
 #define PROG_START 0x0200
 #define SCREEN_WIDTH 64
 #define SCREEN_HEIGHT 32
-#define CPU_SPEED 500 // Hz
+#define TICKS_PER_SEC 500 // Hz (cpu speed)
+#define COUNTS_PER_SEC 60 // Hz (delay and sound timer)
+#define CLOCKS_PER_TICK (CLOCKS_PER_SEC / TICKS_PER_SEC)
+#define CLOCKS_PER_COUNT (CLOCKS_PER_SEC / COUNTS_PER_SEC)
 
 // clang-format off
 byte HEX_DIGITS[5 * 16] = {
@@ -35,16 +38,8 @@ byte HEX_DIGITS[5 * 16] = {
 };
 // clang-format on
 
-#define NNN(opcode) ((opcode) & 0x0FFF)
-#define KK(opcode) ((byte)(opcode))
-#define N(opcode) ((byte)(((opcode) & 0x000F)))
-#define X(opcode) ((byte)(((opcode) & 0x0F00) >> 2))
-#define Y(opcode) ((byte)(((opcode) & 0x00F0) >> 1))
-
-#define NEXT_BYTE(vm) vm->mem[vm->pc++]
-
 typedef struct VirtualMachine {
-    byte mem[0x1000];
+    byte memory[0x1000];
     word pc, i;
 
     byte v[0x10];
@@ -53,14 +48,15 @@ typedef struct VirtualMachine {
     byte sp;
 
     byte delay_timer, sound_timer;
-    clock_t last_tick;
+    clock_t last_clock;
+    uint64_t ticks;
 
     bool is_key_pressed[0x10];
 
-    bool wait_for_key;
-    byte store_key_index;
+    bool is_waiting_for_key;
+    byte waiting_for_key_index;
 
-    bool had_error;
+    bool had_error, is_draw_pending;
 
     bool screen[SCREEN_WIDTH][SCREEN_HEIGHT];
 } VM;
@@ -81,10 +77,12 @@ void clear_display(VM* vm) {
             vm->screen[j][k] = false;
         }
     }
+    vm->is_draw_pending = true;
 }
 
 void load(VM* vm, const char* path) {
     vm->had_error = false;
+    vm->is_draw_pending = true;
 
     vm->pc = PROG_START;
     vm->i = 0x0000;
@@ -94,15 +92,21 @@ void load(VM* vm, const char* path) {
     vm->delay_timer = 0;
     vm->sound_timer = 0;
 
-    vm->wait_for_key = false;
+    vm->is_waiting_for_key = false;
 
-    for(int j = 0; j < 80; j++) {
-        vm->mem[j] = HEX_DIGITS[j];
-    }
+    for(int j = 0; j < 0x10; j++) vm->v[j] = 0x00;
 
-    for(int j = 80; j < PROG_START; j++) {
-        vm->mem[j] = 0x00;
-    }
+    for(int j = 0; j < STACK_SIZE; j++) vm->stack[j] = 0x0000;
+
+    for(int j = 0; j < 0x10; j++) vm->is_key_pressed[j] = false;
+
+    vm->last_clock = clock();
+    vm->ticks = 0;
+    clear_display(vm);
+
+    for(int j = 0; j < 80; j++) vm->memory[j] = HEX_DIGITS[j];
+
+    for(int j = 80; j < PROG_START; j++) vm->memory[j] = 0x00;
 
     FILE* fileptr = fopen(path, "rb"); // Open the file in binary mode
     if(fileptr == NULL) {
@@ -119,25 +123,10 @@ void load(VM* vm, const char* path) {
         char* buffer = (char*)malloc(filelen * sizeof(char));
         fread(buffer, filelen, 1, fileptr); // Read in the entire file
         for(int j = 0; j < filelen; j++) {
-            vm->mem[0x200 + j] = buffer[j];
+            vm->memory[0x200 + j] = buffer[j];
         }
     }
     fclose(fileptr); // Close the file
-
-    for(int j = 0; j < 0x10; j++) {
-        vm->v[j] = 0x00;
-    }
-
-    for(int j = 0; j < STACK_SIZE; j++) {
-        vm->stack[j] = 0x0000;
-    }
-
-    for(int j = 0; j < 0x10; j++) {
-        vm->is_key_pressed[j] = false;
-    }
-
-    vm->last_tick = clock();
-    clear_display(vm);
 }
 
 word join(const byte lo, const byte hi) {
@@ -145,127 +134,125 @@ word join(const byte lo, const byte hi) {
 }
 
 word fetch(VM* vm) {
-    const byte hi = NEXT_BYTE(vm);
-    const byte lo = NEXT_BYTE(vm);
+    const byte hi = vm->memory[vm->pc++];
+    const byte lo = vm->memory[vm->pc++];
     return join(lo, hi);
 }
 
 void execute(VM* vm, word opcode) {
-    // unique opcodes
-    switch(opcode) {
-    case 0x00E0: // CLS
-        clear_display(vm);
-        return;
-    case 0x00EE: // RET
-        vm->pc = vm->stack[--vm->sp];
-        return;
-    }
+    const word nnn = opcode & 0x0FFF;
+    const byte kk = opcode & 0xFF;
+    const byte n = opcode & 0x0F;
+    const byte x = (opcode & 0x0F00) >> 8;
+    const byte y = (opcode & 0x00F0) >> 4;
 
     switch(opcode & 0xF000) {
     case 0x0000: // SYS addr
-        report_error(vm, "'SYS' not (yet) implemented");
+        switch(opcode) {
+        case 0x00E0: // CLS
+            clear_display(vm);
+            return;
+        case 0x00EE: // RET
+            vm->pc = vm->stack[--vm->sp];
+            return;
+        }
+        report_error(vm, "'SYS CALL' not (yet) implemented");
         return;
     case 0x1000: // JP addr
-        vm->pc = NNN(opcode);
+        vm->pc = nnn;
         return;
     case 0x2000: // CALL addr
         if(vm->sp == STACK_SIZE) {
             report_error(vm, "stack overflow");
         }
         vm->stack[vm->sp++] = vm->pc;
-        vm->pc = NNN(opcode);
+        vm->pc = nnn;
         return;
     case 0x3000: // SE Vx, byte
-        if((vm->v[X(opcode)]) == KK(opcode)) {
-            vm->pc += 2;
-        }
+        if((vm->v[x]) == kk) vm->pc += 2;
         return;
     case 0x4000: // SNE Vx, byte
-        if(vm->v[X(opcode)] != KK(opcode)) {
-            vm->pc += 2;
-        }
+        if(vm->v[x] != kk) vm->pc += 2;
         return;
     case 0x5000: // SE Vx, Vy
-        if(vm->v[X(opcode)] == vm->v[Y(opcode)]) {
-            vm->pc += 2;
-        }
+        if(vm->v[x] == vm->v[y]) vm->pc += 2;
         return;
     case 0x6000: // LD Vx, byte
-        vm->v[X(opcode)] = KK(opcode);
+        vm->v[x] = kk;
         return;
     case 0x7000: // ADD Vx, byte
-        vm->v[X(opcode)] += KK(opcode);
+        vm->v[x] += kk;
         return;
     case 0x8000:
         switch(opcode & 0x000F) {
         case 0x0000: // LD Vx, Vy
-            vm->v[X(opcode)] = vm->v[Y(opcode)];
+            vm->v[x] = vm->v[y];
             return;
         case 0x0001: // OR Vx, Vy
-            vm->v[X(opcode)] |= vm->v[Y(opcode)];
+            vm->v[x] |= vm->v[y];
             return;
         case 0x0002: // AND Vx, Vy
-            vm->v[X(opcode)] &= vm->v[Y(opcode)];
+            vm->v[x] &= vm->v[y];
             return;
         case 0x0003: // XOR Vx, Vy
-            vm->v[X(opcode)] ^= vm->v[Y(opcode)];
+            vm->v[x] ^= vm->v[y];
             return;
         case 0x0004: // ADD Vx, Vy
-            vm->v[0xF] = vm->v[X(opcode)] > 0xFF - vm->v[Y(opcode)] ? 1 : 0;
-            vm->v[X(opcode)] += vm->v[Y(opcode)];
+            vm->v[0xF] = vm->v[x] > (0xFF - vm->v[y]) ? 0x01 : 0x00;
+            vm->v[x] += vm->v[y];
             return;
         case 0x0005: // SUB Vx, Vy
-            vm->v[0xF] = vm->v[X(opcode)] > vm->v[Y(opcode)] ? 1 : 0;
-            vm->v[X(opcode)] -= vm->v[Y(opcode)];
+            vm->v[0xF] = (vm->v[x] > vm->v[y]) ? 0x01 : 0x00;
+            vm->v[x] -= vm->v[y];
             return;
         case 0x0006: // SHR Vx {, Vy}
-            vm->v[0xF] = vm->v[X(opcode)] & 0x0001;
-            vm->v[X(opcode)] >>= 1;
+            vm->v[0xF] = vm->v[x] & 0x0001;
+            vm->v[x] >>= 1;
             return;
         case 0x0007: // SUBN Vx, Vy
-            vm->v[0xF] = vm->v[X(opcode)] > vm->v[Y(opcode)] ? 1 : 0;
-            vm->v[X(opcode)] = vm->v[Y(opcode)] - vm->v[X(opcode)];
+            vm->v[0xF] = vm->v[x] > vm->v[y] ? 0x01 : 0x00;
+            vm->v[x] = vm->v[y] - vm->v[x];
             return;
         case 0x000E: // SHL Vx {, Vy}
-            vm->v[0xF] = (vm->v[X(opcode)] & (1 << 7)) > 0 ? 1 : 0;
-            vm->v[X(opcode)] <<= 1;
+            vm->v[0xF] = vm->v[x] >> 7;
+            vm->v[x] <<= 1;
             return;
         }
     case 0x9000: // SNE Vx, Vy
-        if(vm->v[X(opcode)] != vm->v[Y(opcode)]) {
-            vm->pc += 2;
-        }
+        if(vm->v[x] != vm->v[y]) vm->pc += 2;
         return;
     case 0xA000: // LD I, addr
-        vm->i = NNN(opcode);
+        vm->i = nnn;
         return;
     case 0xB000: // JP V0, addr
-        vm->pc = NNN(opcode) + vm->v[0x0];
+        vm->pc = nnn + vm->v[0x0];
         return;
     case 0xC000: // RND Vx, byte
-        vm->v[X(opcode)] = rand() & KK(opcode);
+        vm->v[x] = (rand() % 0x100) & kk;
         return;
     case 0xD000: // DRW Vx, Vy, nibble
-        const byte n = N(opcode);
-        for(byte r = 0; r < 8; r++) {
-            for(byte s = 0; s < n; s++) {
-                const byte x = (vm->v[X(opcode)] + r) % SCREEN_WIDTH;
-                const byte y = (vm->v[Y(opcode)] + s) % SCREEN_HEIGHT;
-                const bool status = (vm->mem[vm->i + s] & (1 << r)) > 0;
-                vm->v[0xF] = vm->screen[x][y] && status;
-                vm->screen[x][y] ^= status;
+        bool collision = false;
+        for(byte xline = 0; xline < 8; xline++) {
+            for(byte yline = 0; yline < n; yline++) {
+                const byte col = (vm->v[x] + xline) % SCREEN_WIDTH;
+                const byte row = (vm->v[y] + yline) % SCREEN_HEIGHT;
+                const bool next = (vm->memory[vm->i + yline] & (1 << (8 - xline))) > 0;
+                collision = collision || (vm->screen[col][row] && next);
+                vm->screen[col][row] ^= next;
             }
         }
+        vm->v[0xF] = collision ? 0x01 : 0x00;
+        vm->is_draw_pending = true;
         return;
     case 0xE000:
         switch(opcode & 0x00FF) {
         case 0x009E: // SKP Vx
-            if((vm->is_key_pressed[vm->v[X(opcode)]])) {
+            if((vm->is_key_pressed[vm->v[x]])) {
                 vm->pc += 2;
             }
             return;
         case 0x00A1: // SKNP Vx
-            if(!(vm->is_key_pressed[vm->v[X(opcode)]])) {
+            if(!(vm->is_key_pressed[vm->v[x]])) {
                 vm->pc += 2;
             }
             return;
@@ -273,38 +260,38 @@ void execute(VM* vm, word opcode) {
     case 0xF000:
         switch(opcode & 0x00FF) {
         case 0x0007: // LD Vx, DT
-            vm->v[X(opcode)] = vm->delay_timer;
+            vm->v[x] = vm->delay_timer;
             return;
         case 0x000A: // LD Vx, K
-            vm->wait_for_key = true;
-            vm->store_key_index = X(opcode);
+            vm->is_waiting_for_key = true;
+            vm->waiting_for_key_index = x;
             return;
         case 0x0015: // LD DT, Vx
-            vm->delay_timer = vm->v[X(opcode)];
+            vm->delay_timer = vm->v[x];
             return;
         case 0x0018: // LD ST, Vx
-            vm->v[X(opcode)] = vm->sound_timer;
+            vm->sound_timer = vm->v[x];
             return;
         case 0x001E: // ADD I, Vx
-            vm->i = (vm->i & vm->v[X(opcode)]) & 0xFFF;
+            vm->i = vm->i & vm->v[x];
             return;
         case 0x0029: // LD F, Vx
-            vm->i = 5 * vm->v[X(opcode)];
+            vm->i = 5 * vm->v[x];
             return;
         case 0x0033: // LD B, Vx
-            const byte vx = vm->v[X(opcode)];
-            vm->mem[vm->i] = vx / 100;
-            vm->mem[vm->i + 1] = (vx % 100) / 10;
-            vm->mem[vm->i + 2] = (vx % 10);
+            const byte vx = vm->v[x];
+            vm->memory[vm->i] = vx / 100;
+            vm->memory[vm->i + 1] = (vx % 100) / 10;
+            vm->memory[vm->i + 2] = (vx % 10);
             return;
         case 0x0055: // LD [I], Vx
-            for(int j = 0; j <= X(opcode); j++) {
-                vm->mem[vm->i + j] = vm->v[j];
+            for(int j = 0; j <= x; j++) {
+                vm->memory[vm->i + j] = vm->v[j];
             }
             return;
         case 0x0065: // LD Vx, [I]
-            for(int j = 0; j <= X(opcode); j++) {
-                vm->v[j] = vm->mem[vm->i + j];
+            for(int j = 0; j <= x; j++) {
+                vm->v[j] = vm->memory[vm->i + j];
             }
             return;
         }
@@ -313,21 +300,15 @@ void execute(VM* vm, word opcode) {
     return;
 }
 
-void cycle(VM* vm) {
-    if(vm->wait_for_key) {
-        report_error(vm, "waiting for key input: not yet implemented");
-    }
-
-    // handle input
-
+void tick(VM* vm) {
     const word opcode = fetch(vm);
     execute(vm, opcode);
 }
 
 void draw(VM* vm) {
-    for(int k = 0; k < SCREEN_HEIGHT; k++) {
-        for(int j = 0; j < SCREEN_WIDTH; j++) {
-            const bool status = vm->screen[j][k];
+    for(int row = 0; row < SCREEN_HEIGHT; row++) {
+        for(int col = 0; col < SCREEN_WIDTH; col++) {
+            const bool status = vm->screen[col][row];
             printf(status ? "." : " ");
         }
         printf("\n");
@@ -335,27 +316,45 @@ void draw(VM* vm) {
     printf("###\n");
 }
 
-void tick(VM* vm) {
-    for(int t = 0; t < 5; t++) {
-        cycle(vm);
-    }
-
-    if(vm->delay_timer) {
-        vm->delay_timer--;
-    }
-
-    if(vm->sound_timer > 0) {
-        vm->sound_timer--;
-        report_warning(vm, "sound not yet implemented");
-    }
-    draw(vm);
-}
-
 void step(VM* vm) {
-    const clock_t curr_tick = clock();
-    if((curr_tick - vm->last_tick) > CLOCKS_PER_SEC / 60) {
+    // vm->is_key_pressed[0x05] = true;
+
+    if(vm->is_waiting_for_key) {
+        report_error(vm, "key inputs not yet implemented");
+        for(byte key = 0; key < 0x10; key++) {
+            if(vm->is_key_pressed[key]) {
+                vm->v[vm->waiting_for_key_index] = key;
+                vm->is_waiting_for_key = false;
+                return;
+            }
+        }
+        return;
+    }
+
+    const clock_t curr_clock = clock();
+    const clock_t delta_clocks = curr_clock - vm->last_clock;
+
+    if(delta_clocks > CLOCKS_PER_TICK * vm->ticks) {
         tick(vm);
-        vm->last_tick = curr_tick;
+        vm->ticks++;
+    }
+
+    if(delta_clocks > CLOCKS_PER_COUNT) {
+        vm->ticks = 0;
+        vm->last_clock = curr_clock;
+
+        if(vm->delay_timer) vm->delay_timer--;
+
+        if(vm->sound_timer > 0) vm->sound_timer--;
+
+        if(vm->sound_timer > 0) {
+            report_warning(vm, "sound not (yet) implemented");
+        }
+
+        if(vm->is_draw_pending) {
+            draw(vm);
+            vm->is_draw_pending = false;
+        }
     }
 }
 
